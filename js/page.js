439e69cdc1a0bc3a8156f2eb03c0616b9a5e7159
Carlxu <carl.zheheng@gmail.com>
2018-06-03 18:23:09 +0800

    20180603

--- js/page.js
+++ js/page.js
@@ -20,3 +20,32 @@ $('.multi-select-bar .option input').bind('change', function () {
   })
   $thisParent.find('.select-value').text(selectedOptions.length > 0 ? selectedOptions.join(',') : '请选择')
 })
+var _winHeight = window.innerHeight;
+let _header = $(".pop-head .header").height();
+let _headList = $(".pop-head .head-list").css("margin-top");
+if (_headList) {
+  _headList = +_headList.substr(0,_headList.length-2);
+}
+$(".pop-head .head-list").height(_winHeight - _header -_headList - 20);
+$(".pop-head .header input").keyup(function(){
+    let _inputValue = $(this).val();
+    $(".pop-head .head-list li").each(function(item,value){
+        if ($(this).text().indexOf(_inputValue) > -1) {
+          $(this).show();
+        }
+        else{
+          $(this).hide();
+        }
+    });
+});
+$(".pop-head .head-list").on("click","li", function(){
+    let _actText = $(this).text();
+    $(".head-content").text(_actText);
+    $(this).parent().parent().hide();
+});
+$(".head-content").click(function(){
+    $(".pop-head").show();
+});
+$(".pop-head .head-back").click(function(){
+  $(this).parent().parent().hide();
+});
